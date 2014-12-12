2eaf8b306054ff74e3128211e04bf8e5bfaf7d3e
adutev <adutev@gmail.com>
2014-12-12 19:49:19 +0200

    Exam-template almost finished. TODO: deleteBookmark, createBookmark

--- Exam_Template/scripts/ajax-requester.js
+++ Exam_Template/scripts/ajax-requester.js
@@ -1,40 +1,83 @@
 var app = app || {};
 
-app.ajaxRequester = (function() {
-	var baseUrl = "https://api.parse.com/1/";
+'use strict';
 
-	var headers = {
+app.ajaxRequester = (function() {
+    var baseUrl = "https://api.parse.com/1/";
+
+   var headers = {
 		"X-Parse-Application-Id": "aN0XX6pFmd8FNhE8lEQNVcYZA8uLx6NJDrg7Qfx1",
 		"X-Parse-REST-API-Key": "8mK9OSAAwrnPiiMKL2fFXso1KP0beIWyCznhLXvU"
 	}
 
-	function login(username, password, success, error) {
-		$.ajax({
-			url: baseUrl + 'login',
-			method: 'GET',
-			headers: headers,
-			data: {
-				username: username,
-				password: password
-			},
-			success: success,
-			error: error
-		})
-	}
-
-	function register(username, password, success, error) {
-		$.ajax({
-            method: "POST",
+    function login(username, password, success, error) {
+        jQuery.ajax({
+            method: "GET",
             headers: headers,
-            url: baseUrl + "users",
+            url: baseUrl + "login",
             data: {username: username, password: password},
             success: success,
             error: error
         });
-	}
-	return {
-		login: login,
-		register: register
-	}
+    }
 
-}());
+    function register(username, password, success, error) {
+        jQuery.ajax({
+            method: "POST",
+            headers: headers,
+            url: baseUrl + "users",
+            data: JSON.stringify({username: username, password: password}),
+            success: success,
+            error: error
+        });
+    }
+
+    function addSessionTokenToHeaders(sessionToken) {
+        var headersWithSessionToken = JSON.parse(JSON.stringify(headers));
+        headersWithSessionToken['X-Parse-Session-Token'] = sessionToken;
+        return headersWithSessionToken;
+    }
+
+    function getBookmarks(sessionToken, success, error) {
+        var headersWithToken = addSessionTokenToHeaders(sessionToken);
+        jQuery.ajax({
+            method: "GET",
+            headers: headersWithToken,
+            url: baseUrl + "classes/Bookmark",
+            success: success,
+            error: error
+        });
+    }
+
+    function createBookmark(title, url, userId, success, error) {
+        var bookmark = {title: title, url: url, ACL : {}};
+        bookmark.ACL[userId] = {"write": true, "read": true};
+        jQuery.ajax({
+            method: "POST",
+            headers: headers,
+            url: baseUrl + "classes/Bookmark",
+            data: JSON.stringify(bookmark),
+            success: success,
+            error: error
+        });
+    }
+
+    function deleteBookmark(sessionToken, bookmarkId, success, error) {
+        var headersWithToken = addSessionTokenToHeaders(sessionToken);
+        jQuery.ajax({
+            method: "DELETE",
+            headers: headersWithToken,
+            url: baseUrl + "classes/Bookmark/" + bookmarkId,
+            success: success,
+            error: error
+        });
+    }
+
+    return {
+        login: login,
+        register: register,
+        getBookmarks: getBookmarks,
+        createBookmark: createBookmark,
+        deleteBookmark: deleteBookmark
+    };
+})();
--- Exam_Template/scripts/app.js
+++ Exam_Template/scripts/app.js
@@ -4,31 +4,80 @@
 		addEventListeners();
 	})
 
+	// Event handlers
 	function addEventListeners() {
-		$('#registerViewButton').on('click', function() {
-			showRegisterView();
-		});
-
-		$('#loginViewButton').on('click', function() {
-			showLoginView();
-		});
-
-		$('#register-button').on('click', function() {
-			registerClicked();
-		});
-
-		$('#login-button').on('click', function() {
-			loginClicked();
-		});
+		var selector = '#main-content';
+		registerViewClicked.call(this, selector);
+		loginViewClicked.call(this, selector);
+		registerClicked.call(this, selector);
+		loginClicked.call(this, selector);
+		logoutClicked.call(this, 'header');
+		deleteBookmarkButtonClicked.call(this, '.bookmark-box');
 	}
 
+	var loginViewClicked = function(selector) {
+		$(selector).on('click', '#loginViewButton', showLoginView);
+	}
+
+	var registerViewClicked = function(selector) {
+		$(selector).on('click', '#registerViewButton', showRegisterView);
+	}
+
+	var loginClicked = function(selector) {
+		$(selector).on('click', '#login-button', function() {
+			var username = $('#username').val();
+			var password = $('#password').val();
+			sessionStorage['username'] = username;
+			app.ajaxRequester.login(username, password,
+				authSuccess, loginError);
+		})
+	}
+
+	var registerClicked = function(selector) {
+		$(selector).on('click', '#register-button', function() {
+			var username = $('#username').val();
+			var password = $('#password').val();
+			sessionStorage['username'] = username;
+			app.ajaxRequester.register(username, password,
+				authSuccess, registerError);
+		})
+	}
+
+	var logoutClicked = function() {
+		$('header').on('click', '#logout-button', function() {
+			$('#logout-button').hide();
+			showLoginView();
+			userSession.logout();
+			showCurrentUser();
+		})
+	}
+
+	var deleteBookmarkButtonClicked = function (selector) {
+		var _this = this;
+		$(selector).on('click', '.delete-bookmark-btn', function (ev) {
+			var deleteConfirmed = confirm('Do you want to delete this bookmark');
+			if (deleteConfirmed) {
+				var objectId = $(this).parent().data('id');
+				_this._data.bookmarks.delete(objectId)
+					.then(function (data) {
+						$(ev.target).parent().remove();
+					},
+					function (error) {
+						console.log(error);
+					})
+			};
+		})
+	}
+
+	// Show login, register and bookmarks views
 	function showLoginView() {
-		var username = $('#username').val('');
-		var password = $('#password').val('');
-		$('#register-button').hide();
+		$('#login').show();
+		var username = $('#username').val('toni');
+		var password = $('#password').val('123');
 		$('#login-button').show();
-		$('#loginViewButton').hide();
 		$('#registerViewButton').show();
+		$('#register-button').hide();
+		$('#loginViewButton').hide();
 	}
 
 	function showRegisterView() {
@@ -40,37 +89,61 @@
 		$('#loginViewButton').show();
 	}
 
-	function registerClicked() {
-		var username = $('#username').val();
-		var password = $('#password').val();
-		app.ajaxRequester.register(username, password,
-			authSuccess, registerError);
+	function showBookmarksView() {
+		$('#login').hide();
+		$('#logout-button').show();
 	}
 
-	function loginClicked() {
-		var username = $('#username').val();
-		var password = $('#password').val();
-		app.ajaxRequester.login(username, password,
-			authSuccess, loginError);
+	function showAllBookmarks(data) {
+		var sessionToken = data.sessionToken;
+		app.ajaxRequester.getBookmarks(sessionToken, getbookmarksSuccess, showErrorMessage);
+	}
+
+	function getbookmarksSuccess(data) {
+		var $bookmarksContainer = $('#bookmarks');
+		$bookmarksContainer.html('');
+
+		for (var b in data.results) {
+			var bookmark = data.results[b];
+			var $bookmarkDiv = $('<div class="bookmark-box">');
+
+			var $title = $('<div class="bookmark-title">');
+			$title.text(bookmark.title);
+			$bookmarkDiv.append($title)
+			
+			var $url = $('<a class="bookmark-url" target="_blank">')
+			$url.text(bookmark.url);
+			$url.attr("href", bookmark.url);
+			$bookmarkDiv.append($url);
+
+			var $deleteButton = $('<a href="#">Delete</a>');
+            $deleteButton.click(deleteBookmarkButtonClicked);
+            $bookmarkDiv.append($deleteButton);
+			$('#bookmarks').append($bookmarkDiv);
+		};
 	}
 
 	function authSuccess(data) {
-
 		userSession.login(data);
 		showCurrentUser(data);
+		showBookmarksView();
+		showAllBookmarks(data);
 	}
 
 	function showCurrentUser(data) {
-		$('h4 span').html('');
-		$('h4 span').append('Current user is: ' + data.username);
+		$('h1 span').html('');
+		if (userSession.getCurrentUser()) {
+			$('h1 span').append('Current user is: ' + userSession.getCurrentUser().username);
+		};
 	}
 
+	// Error messages
 	function loginError(error) {
 		showAjaxError("Login failed", error);
 	}
 
 	function registerError(error) {
-		showAjaxError("Login failed", error);
+		showAjaxError("Register failed", error);
 	}
 
 
@@ -94,7 +167,9 @@
 
 	var userSession = {
 		login: function(data) {
-			sessionStorage['currentUser'] = JSON.stringify(data);
+			var dataWithUsername = JSON.parse(JSON.stringify(data));
+			dataWithUsername['username'] = sessionStorage['username'];
+			sessionStorage['currentUser'] = JSON.stringify(dataWithUsername);
 		},
 		getCurrentUser: function() {
 			var userData = sessionStorage['currentUser'];
@@ -104,6 +179,7 @@
 		},
 		logout: function() {
 			delete sessionStorage['currentUser'];
+			delete sessionStorage['username'];
 		}
 	}
 }())
